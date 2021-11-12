82817bd5da40600dcc9375fcdfb4d14580a3410c
nicooterounibazo <nicolasoterounibazo@gmail.com>
2021-11-12 18:38:33 -0300

    last commit entrega 6

--- entregable 2/js/my-profile.js
+++ entregable 2/js/my-profile.js
@@ -2,28 +2,44 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-    
-    
-});
+    // Obtenemos la data del localStorage y la convertimos a formato json
+    var dataProfile = JSON.parse(localStorage.getItem('data'));
+    if (dataProfile) {
+        
+        /* Si existe data, asigno a cada input su valor correspondiente.
+        */
+        document.getElementById("name").value = dataProfile.name;
+        document.getElementById("lastName").value = dataProfile.lastName;
+        document.getElementById("age").value = dataProfile.age;
+        document.getElementById("email").value = dataProfile.email;
+        document.getElementById("phone").value = dataProfile.phone;
+    }
 
-document.getElementById("guardar").addEventListener("click", function (e) {
-    var nombre = document.getElementById("name").value;
-    var apellido = document.getElementById("lastname").value;
-    var edad = document.getElementById("age").value;
-    var telefono = document.getElementById("tel").value;
-    var email = document.getElementById("email").value;
-    const formulario = {
-        nombre,
-        apellido,
-        edad,
-        telefono,
-        email
-    };
-    console.log(formulario, "aca");
-    
-     if(form != ""){
-            localStorage.setItem("data",JSON.stringify(formulario));
+    // Obtengo el formulario.
+    var form = document.getElementById('profileForm');
 
-     }
-    
+    // Defino el evento submit
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevenir que el formulario se envie.
+
+        // Obtengo el valor de cada input y lo cargo en su variable correspondiente.
+        var name = document.getElementById("name").value;
+        var lastName = document.getElementById("lastName").value;
+        var age = document.getElementById("age").value;
+        var phone = document.getElementById("phone").value;
+        var email = document.getElementById("email").value;
+
+        // Defino un objecto donde guardo toda la data de los campos ingresados para posteriormente guardarlo en localStorage.
+        const formulario = {
+            name,
+            lastName,
+            age,
+            phone,
+            email
+        };
+
+        //Guardo la data en localStorage
+        localStorage.setItem("data", JSON.stringify(formulario));
+        form.classList.add('was-validated');
+    }, false);
 });
